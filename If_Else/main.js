7b2a0069020b902f3f0512a9a709fa980915a96d
kkoahis <khoakun27@gmail.com>
2022-09-02 11:04:14 +0700

    modify Logical_Operator

---
+++ If_Else/main.js
@@ -0,0 +1,15 @@
+
+// Câu lệnh rẽ nhánh if-else
+
+var date = 10;
+
+// dùng else-if thì đúng 1 điều kiện sẽ break luôn, còn nếu dùng if bình thường thì sẽ cứ chạy cho đến hết chương trình 
+if (date === 2) {
+   console.log('Hôm nay là thứ 2');
+} else if(date === 3) {
+   console.log('Hôm nay là thứ 3');
+} else if(date === 4) {
+   console.log('Hôm nay là thứ 4');
+} else{
+   console.log('i dont know');
+}
--- Operator_Datatypes/Logical_Operator2.js
+++ Operator_Datatypes/Logical_Operator2.js
@@ -14,14 +14,13 @@ var b = 2;
 var result = 'A' && 'B' && NaN && 'C';   // Toán tử && sẽ lấy ngay các giá trị false nếu có trong câu
 var result2 = 'A' && 'B' && 'C'; // Nếu không có các giá trị false nó sẽ lấy giá trị cuối
 
-var rs = 'A' || 'B' || 'C' || NaN;   // Toán tử || sẽ lấy ngay sau các giá trị false các giá trị true, nếu giá trị đầu là giá trị true thì lấy luôn
+var rs = 'A' || false || 'C' || 'D' || 'E';   // Toán tử || sẽ lấy ngay sau các giá trị false các giá trị true, nếu giá trị đầu là giá trị true thì lấy luôn
 var rs2 = 'A' || 'B' || 'C'; // Nếu không có các giá trị false nó sẽ lấy giá trị đầu tiên true
 
 console.log('result', result);
 console.log('result2' ,result2);
 console.log('rs', rs);
 console.log('rs2', rs2);
-
 
 if(result){
    console.log('Điều kiện đúng');
@@ -31,7 +30,6 @@ else{
 }
 
 /////////////////////////////////////////////////////////
-
 if(rs){
    console.log('Điều kiện đúng');
 }
